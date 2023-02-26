577ae20f57d1fc88203b69d118e2a3b4dbf1db96
Greg Giles <ggiles@RVMI-0112.redviking.local>
2023-02-26 05:52:08 -0500

    remove gitlab files

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -1,4 +1,4 @@
 npm run build
-docker build -t registry.gitlab.com/redviking/agv-pd/rv-agv-documentation:latest .
-docker push registry.gitlab.com/redviking/agv-pd/rv-agv-documentation:latest 
-kubectl --namespace agv-doc rollout restart deploy agv-doc
+# docker build -t registry.gitlab.com/redviking/agv-pd/rv-agv-documentation:latest .
+# docker push registry.gitlab.com/redviking/agv-pd/rv-agv-documentation:latest 
+# kubectl --namespace agv-doc rollout restart deploy agv-doc
