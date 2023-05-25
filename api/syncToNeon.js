1fbe98a74dd57a1c0f8e08b73143cec4c1c41e66
Greg Giles <ggiles@RVMI-0112.redviking.local>
2023-05-24 22:01:53 -0400

    adds sync from google to Neon

---
+++ api/syncToNeon.js
@@ -0,0 +1,64 @@
+const { google } = require('googleapis');
+const postgres = require('postgres');
+
+const auth = new google.auth.JWT(
+  process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+  null,
+  process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), 
+  ['https://www.googleapis.com/auth/spreadsheets'],
+);
+const sheets = google.sheets({ version: 'v4', auth });
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}`;
+const db = postgres(URL, { ssl: 'require' });
+
+module.exports = async (req, res) => {
+  try {
+    const { data } = await sheets.spreadsheets.values.get({
+      spreadsheetId: process.env.SPREADSHEET_ID,
+      range: process.env.RIDER_RANGE,
+    });
+
+    data.values.shift();
+    data.values.shift();
+    data.values.shift();
+    
+    var riders=[]
+    data.values.forEach((rider) => {
+      const id = Number(rider[0]);
+      const name = rider[1];
+      const type = rider[2];
+      var grade = null;
+      if (rider[5]) {
+        grade = Number(rider[5]);
+      }
+      else {
+        grade = null;
+      }
+      if (rider[0].length > 0)
+        riders.push([id, name, type, grade]);
+    })
+
+    const values = riders.map(([id, name, type, grade], index) => {
+      // Generating placeholders
+      const offset = index * 4;
+      return `($${offset + 1}, $${offset + 2}, $${offset + 3}, $${offset + 4})`;
+    }).join(', ');
+
+    const params = riders.flat();
+    const query = `
+      INSERT INTO riders (id, name, type, grade)
+      VALUES ${values}
+      ON CONFLICT (id) DO UPDATE
+      SET name = EXCLUDED.name, type = EXCLUDED.type, grade = EXCLUDED.grade
+    `;
+  
+    await db.unsafe(query, params);
+
+   res.status(201).json({ message: `${riders.length} Riders syncd successfully` });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
